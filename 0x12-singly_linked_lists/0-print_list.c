4b85e6c18dc1a3c57b42930d5093837efd0a458d
Luk0ye <slukoye01@gmail.com>
2023-08-27 22:00:46 +0300

    0. Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,47 +1,24 @@
 #include <stdio.h>
-#include <stddef.h>
-/*this is the begining of the code.
- * theo
+#include "lists.h"
+
+/**
+ * print_list - Prints all the elements of a linked list
+ * @h: Pointer to the list_t list to print
+ *
+ * Return: The number of nodes printed
  */
-struct list_t
-{
-    char *str;
-    struct list_t *next;
-};
-/*continuation
- the code is coding*/
-
-typedef struct list_t list_t;
-
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
+	size_t s = 0;
 
-    while (h != NULL) {
-        if (h->str == NULL) {
-            printf("[0] (nil)\n");
-        } else {
-            printf("[%d] %s\n", count, h->str);
-        }
-        h = h->next;
-        count++;
-    }
-
-    return count;
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+	s++;
+	}
+	return (s);
 }
-
-int main(void) 
-{
-    list_t node1 = {"Hello", NULL};
-    list_t node2 = {"World", NULL};
-    list_t node3 = {NULL, NULL};
-
-    node1.next = &node2;
-    node2.next = &node3;
-
-    size_t numNodes = print_list(&node1);
-    printf("Total number of nodes: %lu\n", numNodes);
-
-    return 0;
-}
-
